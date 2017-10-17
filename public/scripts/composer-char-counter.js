57573b334ac960e763e903e0acd939681b27e10f
digitalfabric92 <dwawryko@gmail.com>
2017-10-17 16:53:19 -0400

    Implemented the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(function () {
+    // Keyup event tracker on the textarea element
+    $('textarea').on('keyup', function(){
+        // Text area is called
+        let charsN = 280 - $(this).val().length;
+        // 280 used as twitter value
+        // Traversing the DOM to find the element with the class counter
+        // $() jQuery selector required on this
+        // .parent .find jQuery traversing methods chained
+        let counter = $(this).parent().find('.counter');
+        counter.text(charsN);
+
+        if (charsN < 0) { // Error when no chars remaining
+            counter.addClass('error')
+            counter.removeClass('warning')
+        } else {
+            counter.removeClass('error')
+            counter.removeClass('warning')
+        }
+    });
+})
